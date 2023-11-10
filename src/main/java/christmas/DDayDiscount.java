89b9015184a80909768decc176fc9527e881acab
hongseokhodev <hongseokhodev@gmail.com>
2023-11-10 16:05:27 +0900

    refactor: 상수 분리

--- src/main/java/christmas/DDayDiscount.java
+++ src/main/java/christmas/DDayDiscount.java
@@ -1,20 +1,23 @@
 package christmas;
 
 public class DDayDiscount {
+    private final static int INITIAL_AMOUNT = 1000;
+    private final static int ADDEND = 100;
+    private final static int FIRST_DAY = 1;
+    private final static int LAST_DAY = 25;
     private final int date;
-
     public DDayDiscount(int date) {
         validate(date);
         this.date = date;
     }
 
     private void validate(int date) {
-        if (date > 25) {
+        if (date > LAST_DAY) {
             throw new IllegalArgumentException();
         }
     }
 
     public int discountByDate() {
-        return 1000 + (date - 1) * 100;
+        return INITIAL_AMOUNT + (date - FIRST_DAY) * ADDEND;
     }
 }
