59d65f40ad35673c26ef93637a7dec7d8277fccf
hongseokhodev <hongseokhodev@gmail.com>
2023-11-14 03:15:35 +0900

    fix: Giveaway의 detail()이 증정 메뉴의 수량이 아닌 가격을 제대로 담는다

--- src/main/java/christmas/domain/benefit/Giveaway.java
+++ src/main/java/christmas/domain/benefit/Giveaway.java
@@ -6,10 +6,9 @@ import christmas.domain.BenefitDetail;
 public class Giveaway implements Benefit{
     private static final String NAME = "증정 이벤트";
     private static final Menu GIVEAWAY_MENU = Menu.CHAMPAGNE;
-    private static final int GIVEAWAY_AMOUNT = 1;
 
     @Override
     public BenefitDetail detail() {
-        return new BenefitDetail(NAME, GIVEAWAY_AMOUNT);
+        return new BenefitDetail(NAME, GIVEAWAY_MENU.getPrice());
     }
 }
