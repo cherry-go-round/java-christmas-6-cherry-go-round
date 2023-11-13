fee4d48d3d116540e9b76a0474c0a1f4c560204d
hongseokhodev <hongseokhodev@gmail.com>
2023-11-14 03:08:33 +0900

    feat: 할인 전 총주문 금액을 출력한다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,5 +1,6 @@
 package christmas.view;
 
+import java.text.DecimalFormat;
 import java.time.LocalDate;
 import java.util.Map;
 
@@ -7,6 +8,10 @@ public class OutputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String RESULT_TITLE = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
     private static final String MENU_TITLE = "<주문 메뉴>";
+    private static final String UNIT_OF_NUMBER = "개";
+    private static final String UNIT_OF_MONEY = "원";
+    private static final DecimalFormat MONEY_FORMATTER = new DecimalFormat("###.###");
+
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
     }
@@ -21,7 +26,12 @@ public class OutputView {
         menu.keySet()
                 .forEach(name -> printMenuElements(name, menu.get(name)));
     }
+
+    public void printTotalAmountBeforeDiscount(int amount) {
+        System.out.println(MONEY_FORMATTER.format(amount) + UNIT_OF_MONEY);
+    }
+
     private void printMenuElements(String name, int number) {
-        System.out.println(name + " " + number + "개");
+        System.out.println(name + " " + number + UNIT_OF_NUMBER);
     }
 }
