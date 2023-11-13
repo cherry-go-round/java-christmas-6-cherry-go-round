9c0eca7b1dd3f132d2e6efb7d28ad1e63e6bdc02
hongseokhodev <hongseokhodev@gmail.com>
2023-11-14 03:01:52 +0900

    feat: 주문 메뉴 내역을 출력한다

--- src/main/java/christmas/view/OutputView.java
+++ src/main/java/christmas/view/OutputView.java
@@ -1,10 +1,12 @@
 package christmas.view;
 
 import java.time.LocalDate;
+import java.util.Map;
 
 public class OutputView {
     private static final String START_MESSAGE = "안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.";
     private static final String RESULT_TITLE = "%d월 %d일에 우테코 식당에서 받을 이벤트 혜택 미리 보기!";
+    private static final String MENU_TITLE = "<주문 메뉴>";
     public void printStartMessage() {
         System.out.println(START_MESSAGE);
     }
@@ -12,5 +14,14 @@ public class OutputView {
     public void printResultTitle(LocalDate localDate) {
         System.out.printf(RESULT_TITLE, localDate.getMonthValue(), localDate.getDayOfMonth());
         System.out.println();
+    }
+
+    public void printMenu(Map<String, Integer> menu) {
+        System.out.println(MENU_TITLE);
+        menu.keySet()
+                .forEach(name -> printMenuElements(name, menu.get(name)));
+    }
+    private void printMenuElements(String name, int number) {
+        System.out.println(name + " " + number + "개");
     }
 }
