dc657c1a96c76d47831ccc9ec50fab706959bd80
hongseokhodev <hongseokhodev@gmail.com>
2023-11-14 21:20:37 +0900

    feat: 12월 이벤트 배지를 출력한다

--- src/main/java/christmas/domain/Badge.java
+++ src/main/java/christmas/domain/Badge.java
@@ -1,8 +1,18 @@
 package christmas.domain;
 
 public enum Badge {
-    NONE,
-    STAR,
-    TREE,
-    SANTA
+    NONE("없음"),
+    STAR("별"),
+    TREE("트리"),
+    SANTA("산타");
+
+    private final String name;
+
+    Badge(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
